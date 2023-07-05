66d39eed6db3867d7357510fdc868583a9bf5f90
Oliver West <oliversimon.west@gmail.com>
2023-07-05 11:04:19 +0100

    configured and installed tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,17 @@
-// Sort out tailwind config.
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            backgroundImage: {},
+            colors: {
+                midnightBlue: {
+                    100: '#0300A1',
+                    200: '#01004A',
+                    300: '#010021',
+                },
+            },
+        },
+    },
+    plugins: [],
+};
