198bd1f30a0b538ef5d48c906fd0ecee953e6bb2
Oliver West <oliversimon.west@gmail.com>
2023-07-06 13:41:08 +0100

    logo sorted and nav bar looking good

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,30 +1,42 @@
-import { useNavigate, Link } from 'react-router-dom';
+// import { useNavigate, Link } from 'react-router-dom';
 export default function Header() {
     // const navigate = useNavigate();
 
     return (
-        <div className="headerWrapper">
-            <nav className="headerNav">
-                <ul className="headerUL">
-                    <li>
-                        <Link className="headerLink" to={`/`}>
-                            About
-                        </Link>
+        <div className="container mx-auto flex items-center h-24">
+            <a href="_#" className="flex items-center justify-center">
+                <img
+                    className="h-24"
+                    src="https://cdn.discordapp.com/attachments/1039952875394187295/1126492552577695744/celtic_west_logo_v2.png"
+                    alt=""
+                />
+            </a>
+            <nav className="contents font-semibold text-base lg:text-lg">
+                <ul className="mx-auto flex items-center">
+                    <li className="p-5 xl:p-8 active">
+                        <a href="_#">
+                            <span>About</span>
+                        </a>
                     </li>
-                    <li>
-                        <Link className="headerLink" to={`/prompts`}>
-                            Skills
-                        </Link>
+                    <li className="p-5 xl:p-8">
+                        <a href="_#">
+                            <span>Skills</span>
+                        </a>
                     </li>
-                    <li>
-                        <Link className="headerLink" to={`/saved`}>
-                            Projects
-                        </Link>
+                    <li className="p-5 xl:p-8">
+                        <a href="_#">
+                            <span>Projects</span>
+                        </a>
                     </li>
-                    <li>
-                        <Link className="headerLink" to={`/saved`}>
-                            Contact
-                        </Link>
+                    <li className="p-5 xl:p-8">
+                        <a href="_#">
+                            <span>Contact</span>
+                        </a>
+                    </li>
+                    <li className="p-5 xl:p-8">
+                        <a href="_#">
+                            <span>Github</span>
+                        </a>
                     </li>
                 </ul>
             </nav>
