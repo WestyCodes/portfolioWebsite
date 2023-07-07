cb3e0662ca3fd153f9dfd5159224743a4de6bbce
Oliver West <oliversimon.west@gmail.com>
2023-07-07 23:41:42 +0100

    testing navigation options

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from './header/Header.js';
 import Footer from './footer/Footer.js';
 import Main from './main/Main.js';
