4f5bfa4cf36e5e72c3edfe5a090df505ee7743c2
Oliver West <oliversimon.west@gmail.com>
2023-07-11 23:14:06 +0100

    shaping hero section

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,7 +1,12 @@
 export default function Main() {
     return (
         <section className="">
-            <div className="hero section border-solid border-2 border-red-100 h-screen"></div>
+            <div className="border-solid border-2 border-red-100 h-screen">
+                <div className="border-solid border-2 border-red-300 h-screen w-3/4 flex mx-auto">
+                    <div className="right content border-solid border-2 border-red-100 w-1/2"></div>
+                    <div className="left content border-solid border-2 border-red-100 w-1/2"></div>
+                </div>
+            </div>
         </section>
     );
 }
