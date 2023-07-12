0f2c26ec8746e5968c2c97f7c5a2db6ecc6b492a
Oliver West <oliversimon.west@gmail.com>
2023-07-12 11:42:23 +0100

    twcss spacing for hero section

--- src/main/Main.js
+++ src/main/Main.js
@@ -2,9 +2,9 @@ export default function Main() {
     return (
         <section className="">
             <div className="border-solid border-2 border-red-100 h-screen">
-                <div className="border-solid border-2 border-red-300 h-screen w-3/4 flex mx-auto">
-                    <div className="right content border-solid border-2 border-red-100 w-1/2"></div>
-                    <div className="left content border-solid border-2 border-red-100 w-1/2"></div>
+                <div className="border-solid border-2 border-red-300 h-3/4 w-3/4 flex mx-auto mt-10 items-center p-20 gap-10">
+                    <div className="right content border-solid border-2 border-red-100 h-full w-1/2"></div>
+                    <div className="left content border-solid border-2 border-red-100 h-full w-1/2"></div>
                 </div>
             </div>
         </section>
