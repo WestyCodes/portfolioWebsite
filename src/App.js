b61ca6598b62aa54bfa89d79ee89878cafd35662
Oliver West <oliversimon.west@gmail.com>
2023-07-14 12:41:04 +0100

    file name errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import Header from './header/Header.js';
-import Footer from './footer/Footer.js';
-import Main from './main/Main.js';
+// import { useNavigate } from 'react-router-dom';
+
+import Header from './header/header.js';
+import Main from './main/main.js';
+import Footer from './footer/footer.js';
 
 import './App.css';
 
