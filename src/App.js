1f753368927245c8dc215f7a05aa988d9851df8a
Oliver West <oliversimon.west@gmail.com>
2023-08-30 14:33:02 +0100

    setting up routes and project pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 // import { useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Header from './header/header.js';
 import Main from './main/main.js';
 // import Footer from './footer/footer.js';
 
 import './App.css';
+import CaddysHackPage from './caddysHackPage/caddysHackPage.js';
 
 function App() {
     return (
         <div className="App text-midnightBlue-200">
             <Header />
-            <Main />
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/caddyshack" element={<CaddysHackPage />} />
+            </Routes>
             {/* <Footer /> */}
         </div>
     );
---
+++ src/caddysHackPage/caddysHackPage.js
@@ -0,0 +1,9 @@
+export default function CaddysHackPage() {
+    return (
+        <section className="font-semibold text-base lg:text-lg bg-endless-picture">
+            <div>
+                <p className="text-white">Projects Page</p>
+            </div>
+        </section>
+    );
+}
